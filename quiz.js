3c75bc43fff8cf1ea8e3fd71082b9e886cce0e7d
davidchristian2019 <48628872+davidchristian2019@users.noreply.github.com>
2019-05-05 19:38:02 -0700

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,115 @@
+'use strict';
+
+const QUIZ = {
+    questions: [
+        {
+            text: "Which of the following is not a valid JavaScript variable name?",
+            answers: [
+                {
+                    text:"2names",
+                    isCorrect: true,
+                },
+                {
+                    text: "_first_and_last_names",
+                    isCorrect: false,
+                },
+                {
+                    text: "FirstAndLast",
+                    isCorrect: false,
+                },
+                {
+                    text: "None of the above",
+                    isCorrect: false,
+                },
+            ],
+        },
+        {
+            text: "______ tag is an extension to HTML that can enclose any number of JavaScript statements. ",
+            answers: [
+                {
+                    text:"SCRIPT",
+                    isCorrect: true,
+                },
+                {
+                    text: "BODY",
+                    isCorrect: false,
+                },
+                {
+                    text: "HEAD",
+                    isCorrect: false,
+                },
+                {
+                    text: "TITLE",
+                    isCorrect: false,
+                },
+            ],
+        },
+        {
+            text: "How does JavaScript store dates in a date object?",
+            answers: [
+                {
+                    text:"The number of milliseconds since January 1st, 1970",
+                    isCorrect: true,
+                },
+                {
+                    text: "The number of days since January 1st, 1900",
+                    isCorrect: false,
+                },
+                {
+                    text: "The number of seconds since Netscape's public stock offering.",
+                    isCorrect: false,
+                },
+                {
+                    text: "None of the above",
+                    isCorrect: false,
+                },
+            ],
+        },
+        {
+            text: "Which of the following attribute can hold the JavaScript version?",
+            answers: [
+                {
+                    text:"LANGUAGE",
+                    isCorrect: true,
+                },
+                {
+                    text: "SCRIPT",
+                    isCorrect: false,
+                },
+                {
+                    text: "VERSION",
+                    isCorrect: false,
+                },
+                {
+                    text: "None of the above",
+                    isCorrect: false,
+                },
+            ],
+        },
+        {
+            text: "What is the correct JavaScript syntax to write Hello World?",
+            answers: [
+                {
+                    text:"System.out.println(Hello World)",
+                    isCorrect: false,
+                },
+                {
+                    text: "println (Hello World)",
+                    isCorrect: false,
+                },
+                {
+                    text: "document.write(Hello World)",
+                    isCorrect: true,
+                },
+                {
+                    text: "response.write(Hello World)",
+                    isCorrect: false,
+                },
+            ],
+        },
+
+    ],
+
+    answers: [],
+}
+
